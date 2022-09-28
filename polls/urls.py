9da348cc636855d253ece27e48d5b9e9087b5d3a
zoonny <hyungii@naver.com>
2022-09-28 17:58:28 +0900

    update urls.py

--- polls/urls.py
+++ polls/urls.py
@@ -2,13 +2,16 @@ from django.urls import path
 
 from . import views
 
+# app_name = "polls"
 urlpatterns = [
     # /polls/
-    path('', views.index, name='index'),
+    path("", views.index, name="index"),
     # /polls/5
-    path('<int:question_id>/', views.detail, name='detail'),
+    # the 'name' value as called by the {% url %} template tag
+    # path("<int:question_id>/", views.detail, name="detail"),
+    path("specifics/<int:question_id>/", views.detail, name="detail"),
     # /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
+    path("<int:question_id>/results/", views.results, name="results"),
     # /polls/5/vote/
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
